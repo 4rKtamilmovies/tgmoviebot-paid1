f7035bb871509ee78d8e0697463c950cfb0c5c61
Vikramji283 <88868666+Vikramji283@users.noreply.github.com>
2021-12-30 22:01:09 +0530

    Update url_shortner.py

--- plugins/josprojects/url_shortner.py
+++ plugins/josprojects/url_shortner.py
@@ -27,7 +27,7 @@ GPLINKS_API = os.environ.get("GPLINKS_API", "008ccaedd6061ad1948838f410947603de9
 
 reply_markup = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton(text='Join Movies Club', url='https://t.me/+oMiWi94WoAQ0MmY5')
+        InlineKeyboardButton(text='Join Movies Club', url='https://t.me/PMRequestgroup')
         ]]
     )
 
@@ -190,7 +190,7 @@ async def short(link):
     
     # Send the text
     try:
-        shorten_urls += "\n\nmade by @iAmLiKu1 | @iAmLiKu1🔥"
+        shorten_urls += "\n\nmade by @PM_Bots"
         return shorten_urls
     except Exception as error:
         return error
