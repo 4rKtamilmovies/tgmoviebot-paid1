2db32b6b160b02543e250a05579dfb9a78c91ea9
Vikramji283 <88868666+Vikramji283@users.noreply.github.com>
2022-07-24 19:59:47 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
 git clone -b master https://github.com/Vikramji283/tgmoviebot-paid1.git /tgmoviebot-paid1
-cd /tgmoviebot
+cd /tgmoviebot-paid1
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
