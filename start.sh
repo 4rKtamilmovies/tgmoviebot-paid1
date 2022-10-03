1165731a5e1b19808a2515740d656f6742355721
Kim So-hyun <110754631+Mr-BUG-TG@users.noreply.github.com>
2022-10-03 16:50:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,12 @@
-echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/Vikramji283/tgmoviebot-paid1.git /tgmoviebot-paid1
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/4rKtamilmovies/tgmoviebot-paid1.git /tgmoviebot-paid1                       
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /tgmoviebot-paid1
+fi
 cd /tgmoviebot-paid1
-echo "Installing Requirements..."
-pip3 install -U -r requirements.txt
-echo "Starting Bot, Please Wait..."
+pip install -U -r requirements.txt
+echo "Starting Bot....⚡️⚡️"
 python3 bot.py
